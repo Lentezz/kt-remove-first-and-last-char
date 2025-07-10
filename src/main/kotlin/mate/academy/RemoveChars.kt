6c2c218c9f337e23c93276ce3d0131f27c89e1d9
Lentezz <yaroslav.golovan@gmail.com>
2025-07-10 16:45:46 +0300

    Fixed

--- src/main/kotlin/mate/academy/RemoveChars.kt
+++ src/main/kotlin/mate/academy/RemoveChars.kt
@@ -1,8 +1,10 @@
 package mate.academy
 
+const val STRING_MIN_LENGTH = 3
+
 fun removeChars(str: String): String {
     // Implement this function
-    if (str.length < 3) return ""
+    if (str.length < STRING_MIN_LENGTH) return ""
     return str.replaceRange(0, 1, "")
         .replaceRange(str.length - 2, str.length - 1, "")
 }
